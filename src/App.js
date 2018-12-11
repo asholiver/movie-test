9e02ac1cda97350e0cd66f6d1fb856270facc89c
Ashley Oliver <ashleyoliver@Ashleys-MacBook-Pro.local>
2018-12-11 19:41:49 +1100

    hook up initial API

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Container, List, TextField } from "./Components";
+import axios from "axios";
+import { Container, List, Movie, TextField } from "./Components";
 
 class App extends Component {
-  movies = [
-    {
-      Actors: "Macaulay Culkin, Joe Pesci, Daniel Stern, John Heard",
-      Director: "Chris Columbus",
-      Genre: "Comedy, Family",
-      imdbID: "tt0099785",
-      Language: "English",
-      Plot:
-        "It is Christmas time and the McCallister family is preparing for a vacation in Paris, France. But the youngest in the family named Kevin got into a scuffle with his older brother Buzz and was sent to his room which is on the third floor of his house. Then, the next morning, while the rest of the family was in a rush to make it to the airport on time, they completely forgot about Kevin who now has the house all to himself. Being home alone was fun for Kevin, having a pizza all to himself, jumping on his parents' bed, and making a mess. Then, Kevin discovers about two burglars, Harry and Marv, about to rob his house on Christmas Eve. Kevin acts quickly by wiring his own house with makeshift booby traps to stop the burglars and to bring them to justice.",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMzFkM2YwOTQtYzk2Mi00N2VlLWE3NTItN2YwNDg1YmY0ZDNmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
-      Runtime: "103 min",
-      Title: "Home Alone",
-      Year: "1990"
-    },
-    {
-      Actors: "Billy Bob Thornton, Tony Cox, Brett Kelly, Lauren Graham",
-      Director: "Terry Zwigoff",
-      Genre: "Comedy, Crime, Drama",
-      imdbID: "tt0307987",
-      Language: "English",
-      Plot:
-        "Willie T. Stokes is a convicted con man who's led a miserable life. He drinks heavily and constantly embarrasses himself publicly. He only works once a year dressed as Santa. But then come Christmas Eve, he and his pint-sized helper dwarf Marcus stage elaborate robberies and take their department stores for everything they got. This year, they hit a mall in suburban Phoenix, Arizona. This time around, Willie gets distracted by having sex with large women, a bartender who is attracted to Santas, and a kid who's convinced he's the real deal. However, this time around Marcus must once again put up with Willie's heavy drinking and a series of incidents that constantly shoot themselves in the foot. Not to mention a nosy department store security guard who's onto them and wants his cut of the loot. Will Willie and Marcus make it to next Christmas? Or will this be the year the dynamic duo finally face justice?",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjA4Njg1MDcwN15BMl5BanBnXkFtZTYwMzAxNjM3._V1_SX300.jpg",
-      Runtime: "91 min",
-      Title: "Bad Santa",
-      Year: "2003"
-    },
-    {
-      Actors: "Arnold Schwarzenegger, Sinbad, Phil Hartman, Rita Wilson",
-      Director: "Brian Levant",
-      Genre: "Comedy, Family",
-      imdbID: "tt0116705",
-      Language: "English",
-      Plot:
-        "Meet Howard Langston, a salesman for a mattress company is constantly busy at his job, and he also constantly disappoints his son, after he misses his son's karate exposition, he tries hard to come up with a way to make it up to him, this is when his son tells Howard that he wants for Christmas is an action figure of his son's television hero, Turbo Man. Unfortunately for Howard, it is Christmas Eve, and every store is sold out of Turbo Man figures, now Howard must travel all over town and compete with everybody else including a mail man named Myron to find a Turbo Man action figure, and to make it to the Wintertainment parade which will feature Turbo Man.",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BMmJlYzViNzctMjQ1Ni00ZWQ4LThkN2YtMzI2ZGU5Nzk0NTAyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-      Runtime: "89 min",
-      Title: "Jingle All the Way",
-      Year: "1996"
-    }
-  ];
   state = {
     newSearch: "",
     activeMovie: [],
     isActive: false,
     isLoading: false,
-    movies: this.movies,
-    movieList: this.movies
+    movieList: [],
+    totalResults: 0
+  };
+
+  componentDidMount = () => {
+    axios
+      .get("http://www.omdbapi.com/?s=big&apikey=64d9571e")
+      .then(response => {
+        console.log(response.data.Search);
+        this.setState({
+          movieList: response.data.Search,
+          totalResults: Number(response.data.totalResults)
+        });
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
   };
 
   findMovieById = (array, id) => {
@@ -90,7 +63,13 @@ class App extends Component {
   };
 
   render() {
-    const { activeMovie, isActive, movieList, isLoading } = this.state;
+    const {
+      activeMovie,
+      isActive,
+      movieList,
+      isLoading,
+      totalResults
+    } = this.state;
 
     return (
       <Container>
@@ -122,43 +101,9 @@ class App extends Component {
             </Container>
           </div>
           <div className="l-layout--right">
+            <p>{totalResults}</p>
             <Container>
-              {isActive === true ? (
-                <div className="c-movie-container">
-                  <div className="c-movie__item">
-                    <h3>{activeMovie.Title}</h3>
-                    <p>{activeMovie.Genre}</p>
-                    <p>{activeMovie.Plot}</p>
-                    <p>
-                      <strong>Language: </strong>
-                      {activeMovie.Language}
-                    </p>
-                    <p>
-                      <strong>Director: </strong>
-                      {activeMovie.Director}
-                    </p>
-                    <p>
-                      <strong>Year: </strong>
-                      {activeMovie.Year}
-                    </p>
-                    <p>
-                      <strong>Actors: </strong>
-                      {activeMovie.Actors}
-                    </p>
-                    <p>
-                      <strong>Runtime: </strong>
-                      {activeMovie.Runtime}
-                    </p>
-                  </div>
-                  <div className="c-movie__item">
-                    <img
-                      className="c-image"
-                      alt={activeMovie.Title}
-                      src={activeMovie.Poster}
-                    />
-                  </div>
-                </div>
-              ) : null}
+              {isActive === true ? <Movie data={activeMovie} /> : null}
             </Container>
           </div>
         </div>
