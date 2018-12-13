0aa1f8d834a702481c98ca9dccf282763a122f1c
Ashley Oliver <ashleyoliver@Ashleys-MacBook-Pro.local>
2018-12-13 19:31:02 +1100

    size

--- src/Components/EmptyElement/EmptyElement.js
+++ src/Components/EmptyElement/EmptyElement.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./EmptyElement.css";
+import classNames from "classnames";
 
-const EmptyElement = ({ text }) => {
+const EmptyElement = ({ text, size }) => {
+  const classes = classNames({
+    "c-empty-element": true,
+    [`c-empty-element--${size}`]: size == null ? false : true
+  });
   return (
-    <div className="c-empty-element">
+    <div className={classes}>
       <p>{text}</p>
     </div>
   );
