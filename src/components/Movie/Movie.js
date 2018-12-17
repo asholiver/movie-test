ea4b33da8e6d62af8a3327ffe750a698a9dea7c4
Ash Oliver <ashleyioliver.1987@gmail.com>
2018-12-18 10:31:09 +1100

    objectMap util

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
 import "./Movie.css";
 import { Image, Heading } from "./../../elements";
+import { objectMap } from "./../../utils";
 import { MovieDetail, ImagePlaceholder } from "..";
+import { API_EMPTY_TEXT } from "./../../constants";
 
 const Movie = ({ data }) => {
   return (
@@ -10,12 +12,12 @@ const Movie = ({ data }) => {
         <Heading text={data.Title} />
         <p className="c-movie__info">{data.Genre}</p>
         <p className="c-movie__summary">{data.Plot}</p>
-        {Object.keys(data.Details).map((k, index) => (
-          <MovieDetail key={index} title={k} detail={data.Details[k]} />
+        {objectMap(data.Details, (key, value, index) => (
+          <MovieDetail key={index} title={key} detail={value} />
         ))}
       </div>
       <div className="c-movie__item">
-        {data.Poster !== "N/A" ? (
+        {data.Poster !== API_EMPTY_TEXT ? (
           <Image alt={data.Title} src={data.Poster} />
         ) : (
           <ImagePlaceholder />
--- src/utils/objectMap.js
+++ src/utils/objectMap.js
@@ -1,5 +1,5 @@
 const objectMap = (obj, func) => {
-  Object.keys(obj).map((k, index) => func(k, obj[k], index));
+  return Object.keys(obj).map((k, index) => func(k, obj[k], index));
 };
 
 export default objectMap;
