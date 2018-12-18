7b2faca9070c685d734505ea7a4d17fd015e311a
Ash Oliver <ashleyioliver.1987@gmail.com>
2018-12-18 15:36:51 +1100

    tweaks

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const API_ROOT = "http://www.omdbapi.com/";
 
-const API_KEY = "apikey=64d9571e";
+const API_KEY = "64d9571e";
 
-const API_URL = `${API_ROOT}?${API_KEY}&`;
+const API_URL = `${API_ROOT}?apikey=${API_KEY}&`;
 
 export default API_URL;
